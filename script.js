444ebad992b1a747a215b70f62ecffc529fd4a88
mrkasore <mrkasore@gmail.com>
2022-12-11 20:52:34 +0300

    HTML

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let choiceComp = ["rock", "paper", "scissor"];
+    let choiceComp = ["Rock", "Paper", "Scissors"];
     let rand = Math.floor(Math.random() * 3);
     return choiceComp[rand];
 }
@@ -7,48 +7,76 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if(computerSelection === playerSelection) {
         return ("We`re even");
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
+    } else if (computerSelection === "Rock" && playerSelection === "Paper") {
         return ("You win!");
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
+    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
         return ("You lose!");
-    } else if (computerSelection === "paper" && playerSelection === "scissor") {
+    } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
         return ("You win!");
-    } else if (computerSelection === "scissor" && playerSelection === "paper") {
+    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
         return ("You lose!");
-    } else if (computerSelection === "scissor" && playerSelection === "rock") {
+    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
         return ("You win!");
-    } else if (computerSelection === "rock" && playerSelection === "scissor") {
+    } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
         return ("You lose!");
     }
 }
+
+let x_win = 0;
+let x_lose = 0;
 
 function game() {
-    let arr = [];
-    let x_win = 0;
-    let x_lose= 0;
-
-    for (let i = 0; i < 5; i ++) {
-        const playerSelection = "rock";
-        const computerSelection = getComputerChoice();
-        arr.push(playRound(playerSelection, computerSelection));
-        console.log(playRound(playerSelection, computerSelection));
-        if (arr[i] === "You win!")  {
+    if (arr.length < 5) {
+        if (arr[arr.length-1] === "You win!") {
             x_win++;
-        } else if (arr[i] === "You lose!") {
+        }
+        else if (arr[arr.length-1] === "You lose!") {
             x_lose++;
         }
-    }
-    if (x_win > x_lose) {
-        return("You are winner! Congratulations!");
-    }
-    else if (x_win == x_lose){
-        return("Opps! Almost");
+
+        document.querySelector('.score').innerHTML = `${x_win} - ${x_lose}`;
+
+        return;
     } else {
-        return("U suck lol");
+        if (x_win > x_lose) {
+            document.querySelector('.fiveRounds').innerHTML = "You are winner! Congratulations!";
+        }
+        else if (x_win == x_lose){
+            document.querySelector('.fiveRounds').innerHTML = "Opps! Almost";
+        } else {
+            document.querySelector('.fiveRounds').innerHTML = "U suck lol";
+        }
+
     }
 }
 
-//const playerSelection = "rock";
-//const computerSelection = getComputerChoice();
-console.log(game());
-//console.log(playRound(playerSelection, computerSelection));
+const btns = document.querySelectorAll('.btn');
+let n = 0;
+let arr = [];
+
+btns.forEach(btn => btn.addEventListener('click', function(e) {
+    n++;
+    let player = e.target.innerHTML;
+    let computer = getComputerChoice();
+    playRound(player, computer);
+    const result = playRound(player, computer);
+    document.querySelector('.showResults').innerHTML = result;
+
+    arr.push(result);
+
+    game();
+
+    document.querySelector('.showRound').innerHTML = `Round ${n}`;
+
+    if (n == 5) {
+        game();
+        let q = confirm("Try again?");
+        if (q) {
+            location.reload();
+        }
+    }
+}));
+
+document.querySelector('#reload').addEventListener('click', function() {
+    location.reload();
+})
